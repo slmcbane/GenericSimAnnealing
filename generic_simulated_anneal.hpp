6c32ceab09620a2558f24d1fc6184392052f7a14
Sean McBane <seanmcb94@gmail.com>
2016-03-30 01:47:36 -0500

    I've tested and verified this version of the algo correct; it's possible this is the final version but I may still make optimization changes if I think of anything.

--- generic_simulated_anneal.hpp
+++ generic_simulated_anneal.hpp
@@ -1,7 +1,31 @@
+/* This file copyright (c) 2016 Sean McBane, under the terms of the MIT
+   License:
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ */
+
 #ifndef GENERIC_SIM_ANNEALING_HPP
 #define GENERIC_SIM_ANNEALING_HPP
 
 #include <cstdlib>
+#include <iostream>
 
 #ifdef __FAST_MATH__
 #include <cmath>
@@ -12,13 +36,11 @@ using std::size_t;
 /* This struct holds the results from a run of simulated annealing.
  * Fields are as follows:
  *   + best: The best solution the algorithm found.
- *   + nextbest: The second best solution - I may remove this field if 
- *     it's not useful.
  *   + finalcost: The final value of the cost function (for best solution).
  *   + function_evals: Number of cost function evaluations.
  *   + iterations: Number of iterations performed (where an iteration is 
  *     defined as the period between one temperature change and the next).
-*/
+ */
 template<class solution, typename cost_t>
 struct GenSimAnnealResults {
   solution best;
@@ -41,75 +63,97 @@ struct GenSimAnnealParams {
   size_t max_temps;
   size_t iters_per_temp;
   double alpha;
-  double cost_reduction_tol; 
+  double cost_reduction_tol;
   bool verbose;
 };
 
 /* Default values for the simulation parameters for convenience; some or
  * all of these may not be appropriate for a given problem. Use with caution!
  */
-const struct GenSimAnnealParams DEFAULT_SIM_ANNEALING_PARAMS = 
-  { 100, 500, 0.9, 0.0001, false };
+const struct GenSimAnnealParams DEFAULT_SIM_ANNEALING_PARAMS =
+{ 100, 500, 0.9, 0.0001, false };
 
 /* The main routine. Templated on a 'solution' class (must be provided by the
  * user) and a typename 'cost_t' which is the variable type returned by a
  * 'solution''s cost() method. This is the value that the algorithm attempts
- * to minimize. The third template parameter is a function which should accept
+ * to minimize.
+ *
+ * The third template parameter is a function which should accept
  * two arguments of type cost_t, with the second greater than the first, and
- * return a probability that the second is selected over the first. The 
- * implementation of this function is left to the user since a suitable 
- * function may vary with the problem, and it did not seem to belong in the
- * 'solution' class.
+ * a double, returning a probability that the second cost is selected over
+ * the first. The implementation of this function is left to the user.
+ *
+ * It is important to note that this is a different approach than many other 
+ * implementations, which use a fixed probability function and let the 
+ * temperature function be specified on a per problem basis; in this version 
+ * the temperature ALWAYS begins at 1.0 and tends to 0.
  *
  * The 'solution' class must expose two methods; one is 'perturb()', which 
- * should return a slightly and randomly modified copy of the solution object.
+ * should slightly and randomly modify the solution in place.
  * The second is 'cost()' which returns a single value of type cost_t (must be
  * an integral type).
+ *
+ * There are a couple of practical concerns for the user to consider; first,
+ * the cost function needs to be relatively cheap to evaluate for large
+ * problems as it will be evaluated millions of times. Second, and less
+ * obviously, the routine makes a copy of the current solution at every
+ * iteration when it perturbs it. This means that if your 'solution' class
+ * has any data that gets deep copied during assignment (most likely a
+ * std::vector) there will be a large overhead. Use pointers to any data
+ * in the object that does not need to change from iteration to iteration
+ * to avoid this.
  */
-template <class solution, typename cost_t, 
-          double (*acceptance_f)(cost_t, cost_t, double)>
-GenSimAnnealResults<solution, cost_t> genericSimulatedAnneal
-(const solution& s0, const GenSimAnnealParams& params) {
+template<class solution, typename cost_t,
+         double (*acceptance_f)(cost_t, cost_t, double)>
+
+GenSimAnnealResults<solution, cost_t>
+genericSimulatedAnneal(const solution& s0, const GenSimAnnealParams& params) {
 
   // Initialization.
-  size_t max_temp = params.max_temps;
+  size_t max_temps = params.max_temps;
   size_t iters_per_temp = params.iters_per_temp;
   double alpha = params.alpha;
-  double tol   = params.cost_reduction_tol;
+  double tol = params.cost_reduction_tol;
   bool verbose = params.verbose;
   size_t fevals = 0;
+  cost_t cost_init = s0.cost();
+  cost_t cost_old = cost_init;
 
   // Make a copy of the solution that was passed in:
   solution s = s0;
 
   // Keep track of current best solution;
   solution best = s0;
+  cost_t best_cost = cost_init;
 
   // Main loop.
+  double temp = 1.0;
 
   // Iterate over temperatures (main iteration).
   for (size_t outer_iter = 0; outer_iter < max_temps; ++outer_iter) {
     // Inner iterations at each temperature;
     for (size_t inner_iter = 0; inner_iter < iters_per_temp; ++inner_iter) {
       // Perturb the solution:
-      solution s1 = s.perturb();
+      solution s1 = s;
+      s1.perturb();
 
       // Whether or not we keep the new solution;
       bool accepted = false;
 
-      // Compute costs of old and new solution.
-      cost_t cost_old = s.cost();
+      // Compute cost of new solution.
       cost_t cost_new = s1.cost();
-      fevals += 2;
+      fevals ++;
 
       // If new cost is less than old, automatically keep this solution.
       // Also update the stored best solution.
-      if (cost_new < cost_old) { 
+      if (cost_new < cost_old) {
         accepted = true;
-        if (best.cost() > cost_new) best = s1;
-        ++fevals;
+        if (best_cost > cost_new) {
+          best = s1;
+          best_cost = cost_new;
+        }
       }
-      
+
       // Otherwise, with some non-zero probability we accept the increase.
       else {
         double acceptance_prob = acceptance_f(cost_old, cost_new, temp);
@@ -125,45 +169,48 @@ GenSimAnnealResults<solution, cost_t> genericSimulatedAnneal
         if (std::isnan(acceptance_prob)) acceptance_prob = 0;
 #endif /* __FAST_MATH__ */
 
-        double x = (double(rand()))/RAND_MAX;
-        if (acceptance_prob > x) accepted = true;
+        double x = (double(rand())) / RAND_MAX;
+        if (acceptance_prob > x)
+          accepted = true;
       }
 
       // If solution was accepted, update.
       if (accepted) {
         s = s1;
+        cost_old = cost_new;
 
-        if (verbose) std::cout << "Updating solution at outer iteration "
-          << outer_iter << ", inner iteration " << inner_iter << std::endl
-          << "  New cost: " << cost_new << "  Old cost: " << cost_old;
+        if (verbose)
+          std::cout << "Updating solution at outer iteration " << outer_iter
+              << ", inner iteration " << inner_iter << std::endl
+              << "  New cost: " << cost_new << "  Old cost: " << cost_old
+              << std::endl;
 
         // Met the cost reduction criterion; break.
         if ((double(cost_new)) / cost_init < tol) {
+          if (verbose)
+            std::cout << "Met cost reduction criterion at "
+                << "outer iteration " << outer_iter << ", inner iteration "
+                << inner_iter << ".\n";
 
-          if (verbose) std::cout << "Met cost reduction criterion at "
-           << "outer iteration " << outer_iter << ", inner iteration "
-            << inner_iter << ".\n";
-
-          GenSimAnnealingResults res = 
-          { s, s.cost(), fevals, outer_iter };
+          GenSimAnnealResults<solution, cost_t> res =
+            { s, s.cost(), fevals, outer_iter };
           return res;
         }
       }
     }
-    temp = temp*alpha;
+    temp = temp * alpha;
   }
 
-  if (verbose) std::cout << "Completed " << max_temps << " iterations without"
-    << " meeting convergence criterion;\nreturning best value found so far."
-      << std::endl;
+  if (verbose)
+    std::cout << "Completed " << max_temps << " iterations without"
+        << " meeting convergence criterion;\nreturning best value found so far."
+        << std::endl;
 
   // When returning, check to see if the stored best solution is better than
   // the current one and if it is, return it instead.
-  GenSimAnnealingResults res = { s.cost() < best.cost() ? s : best,
-    s.cost() < best.cost() ? s.cost() : best.cost(),
-    fevals, outer_iter };
+  GenSimAnnealResults<solution, cost_t> res = { s.cost() < best.cost() ? s : best,
+      s.cost() < best.cost() ? s.cost() : best.cost(), fevals, max_temps };
   return res;
 }
-
 
 #endif /* GENERIC_SIM_ANNEALING_HPP */
