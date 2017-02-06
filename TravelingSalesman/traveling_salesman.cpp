cfea4415b59baa29bf3b14f78de0b828efa42bc5
Sean McBane <seanmcb94@gmail.com>
2017-02-06 12:50:41 -0600

    Removed duplicate code, rewrote with generic random number generator

--- TravelingSalesman/traveling_salesman.cpp
+++ TravelingSalesman/traveling_salesman.cpp
@@ -1,132 +1,148 @@
-/* Example use case for GenericSimAnneal, solving the classic NP-complete
+/* 
+ * Example use case for GenericSimAnneal, solving the classic NP-complete
  * problem of the traveling salesman who is trying to visit a set of cities
  * in a closed loop with the least possible distance covered. Please note
- * that the acceptance probability function here should not be taken as a
- * reference as to what makes a good one - it's just a guess at something
- * about right. It works decently.
+ * that the acceptance probability function and the cooling schedule here
+ * have the standard forms - see 
+ * <https://en.wikipedia.org/wiki/Simulated_annealing>; however, other forms
+ * for these functions are easily implemented due to the generic nature of 
+ * the implementation.
  *
- * Note the use of a shared_ptr<vector<long> > to hold the coordinates -
- * this avoids the deep copy of the vectors that would be performed at each
- * inner iteration otherwise.
+ * Note the use of shared_ptr's to hold the vectors of coordinates and the
+ * internal random number generator in the Tour class; this way the objects
+ * are not deep copied at every iteration as they would be otherwise.
  *
  * This code is released as an example under no copyright whatsoever.
  */
 
-#include "generic_simulated_anneal.hpp"
-#include <ctime>
+#include "../generic_simulated_anneal.hpp"
 #include <vector>
 #include <cassert>
 #include <cmath>
+#include <chrono>
 #include <memory>
+#include <random>
+
+using std::chrono::system_clock;
 
 // A class representing a particular tour.
 // xcoords and ycoords hold the x- and y- coordinates of all the cities.
 // visited is a vector of the order in which cities are visited (beginning
 // and ending with city # 0).
-class Tour {
+class Tour
+{
+private:
+	std::shared_ptr<std::mt19937> generator;
 public:
-  size_t numcities;
-  std::shared_ptr<std::vector<long>> xcoords, ycoords;
-  std::vector<size_t> visited;
+	size_t numcities;
+	std::shared_ptr<const std::vector<long>> xcoords, ycoords;
+	std::vector<unsigned> visited;
 
-  Tour(std::vector<long> xs, std::vector<long> ys);
-  unsigned long cost() const;
-  void perturb();
+	Tour(const std::vector<long>& xs, const std::vector<long>& ys);
+	unsigned long cost() const;
+	void perturb();
 };
 
-// Constructor.
-Tour::Tour(std::vector<long> xs, std::vector<long> ys) {
-  assert(xs.size() == ys.size());
-  numcities = xs.size();
-
-  xcoords = std::make_shared <std::vector<long>> (std::vector<long>(numcities));
-  ycoords = std::make_shared <std::vector<long>> (std::vector<long>(numcities));
-
-  visited = std::vector<size_t>(numcities + 1);
-  for (size_t i = 0; i < numcities; ++i) {
-    (*xcoords)[i] = xs[i];
-    (*ycoords)[i] = ys[i];
-    visited[i] = i;
-  }
-  visited[numcities] = 0;
+Tour::Tour(const std::vector<long>& xs, const std::vector<long>& ys)
+{
+	assert(xs.size() == ys.size());
+	numcities = xs.size();
+	xcoords = std::make_shared<const std::vector<long>>(xs);
+	ycoords = std::make_shared<const std::vector<long>>(ys);
+	visited = std::vector<unsigned>(numcities + 1);
+	for (unsigned i = 0; i < numcities; ++i)
+		visited[i] = i;
+	visited[numcities] = 0;
+	generator = std::make_shared<std::mt19937>(
+		system_clock::to_time_t(system_clock::now()));
 }
 
 // Computes the cost of the tour as given by the floor of the
 // Pythagorean distance.
-unsigned long Tour::cost() const {
-  unsigned long dist = 0;
-  const std::vector<long>& x = *xcoords;
-  const std::vector<long>& y = *ycoords;
-  for (size_t i = 0; i < numcities; ++i) {
-    long dx = x[visited[i + 1]] - x[visited[i]];
-    long dy = y[visited[i + 1]] - y[visited[i]];
-    double d = sqrt(double(dx * dx + dy * dy));
-    dist += (unsigned long) floor(d);
-  }
-  return dist;
+unsigned long Tour::cost() const
+{
+	unsigned long dist = 0;
+	const std::vector<long>& x = *xcoords;
+	const std::vector<long>& y = *ycoords;
+	for (size_t i = 0; i < numcities; ++i) {
+		long dx = x[visited[i + 1]] - x[visited[i]];
+		long dy = y[visited[i + 1]] - y[visited[i]];
+		double d = sqrt(double(dx * dx + dy * dy));
+		dist += (unsigned long)(floor(d));
+	}
+	return dist;
 }
 
 // Swaps two random cities on the tour; this is a good enough perturbation
 // function for an example case.
-void Tour::perturb() {
-  size_t city1 = 1
-      + (unsigned long) ceil((double(rand())) / RAND_MAX * (numcities - 2));
-  size_t city2 = city1;
-  while (city2 == city1)
-    city2 = 1
-        + (unsigned long) ceil((double(rand())) / RAND_MAX * (numcities - 2));
+void Tour::perturb()
+{
+	auto& gen = *generator;
+	unsigned city1 = 1 + unsigned(floor((double(gen()) * (numcities - 1))
+										/ gen.max()));
+	unsigned city2 = city1;
+	while (city2 == city1)
+		city2 = 1 + unsigned(floor((double(gen()) * (numcities - 1))
+								   / gen.max()));
 
-  size_t temp = visited[city1];
-  visited[city1] = visited[city2];
-  visited[city2] = temp;
+	unsigned temp = visited[city1];
+	visited[city1] = visited[city2];
+	visited[city2] = temp;
 }
 
 // And here is the demonstration of how the algorithm is then used - remember
 // to call srand at the beginning of your code; even if your perturbation 
 // function does something else the annealing routine uses rand() to decide
 // whether or not to accept an increase in cost.
-int main() {
-  srand(time(NULL));
+int main()
+{
+	std::vector<long> xs =
+	{ 0, 194, 908, 585, 666, 76, 633, 963, 789, 117, 409, 257, 229, 334, 837,
+		382, 921, 54, 959, 532, 934, 720, 117, 519, 933, 408, 750, 465, 790,
+		983, 605, 314, 272, 902, 340, 827, 915, 483, 466, 451, 698 };
+	std::vector<long> ys = { 0, 956, 906, 148, 196, 59, 672, 801, 752, 620, 65,
+		747, 377, 608, 374, 841, 910, 903, 743, 477, 794, 973, 555, 496, 152, 52,
+		3, 174, 890, 861, 790, 430, 149, 674, 780, 507, 187, 931, 503, 435, 569 };
+	Tour mytour(xs, ys);
 
-  std::vector<long> xs =
-      { 0, 194, 908, 585, 666, 76, 633, 963, 789, 117, 409, 257, 229, 334, 837,
-          382, 921, 54, 959, 532, 934, 720, 117, 519, 933, 408, 750, 465, 790,
-          983, 605, 314, 272, 902, 340, 827, 915, 483, 466, 451, 698 };
-  std::vector<long> ys = { 0, 956, 906, 148, 196, 59, 672, 801, 752, 620, 65,
-      747, 377, 608, 374, 841, 910, 903, 743, 477, 794, 973, 555, 496, 152, 52,
-      3, 174, 890, 861, 790, 430, 149, 674, 780, 507, 187, 931, 503, 435, 569 };
-  Tour mytour(xs, ys);
+	GenSimAnnealParams params;
+	double alpha;
 
-  // Initialize parameters with default values;
-  GenSimAnnealParams params = DEFAULT_SIM_ANNEALING_PARAMS;
+	std::cout << "Enter max temps: ";
+	std::cin >> params.max_temps;
+	std::cout << "Enter iterations per temperature: ";
+	std::cin >> params.iters_per_temp;
+	std::cout << "Enter alpha: ";
+	std::cin >> alpha;
+	params.verbose = false;
+	params.cost_reduction_tol = 0.0;
 
-  std::cout << "Enter max temps: ";
-  std::cin >> params.max_temps;
-  std::cout << "Enter iterations per temperature: ";
-  std::cin >> params.iters_per_temp;
-  std::cout << "Enter alpha: ";
-  std::cin >> params.alpha;
+	// This lambda is the cooling schedule; for this example, temperature is
+	// reduced by a fixed multiplier alpha at each iteration.
+	auto s = [alpha] (unsigned iter) { return 1.0 * pow(alpha, iter); };
 
-  // This lambda is our acceptance probability function. Note that this is the 
-  // same as the 'canonical' probability function often found in simulated
-  // annealing implementations except for the addition of a scaling factor for
-  // temperature (since T is always 1.0 or less).
-  auto a = [] (unsigned long c1, unsigned long c2, double T) {
-     return exp((static_cast<double>(c1)-static_cast<double>(c2))/T/600.0);
-  };
+	// This lambda is our acceptance probability function. Note that this is the 
+	// same as the 'canonical' probability function often found in simulated
+	// annealing implementations except for the addition of a scaling factor for
+	// temperature (since T is always 1.0 or less).
+	auto a = [] (unsigned long c1, unsigned long c2, double T) {
+		return exp((double(c1) - double(c2)) / T / 600.0);
+	};
 
-  // This is how the routine can be called if a lambda expression is used for
-  // the acceptance function; it's very similar for the case of a functor or a 
-  // normal function declaration.
-  GenSimAnnealResults<Tour, unsigned long> results =
-    genericSimulatedAnneal<Tour, unsigned long, decltype(a)>(mytour, params, a);
+	std::mt19937 generator(system_clock::to_time_t(system_clock::now()));
 
-  std::cout << "Tour length: " << results.finalcost << std::endl;
-  std::cout << "Function evaluations: " << results.function_evals << std::endl;
-  std::cout << "Computed tour: ";
-  for (size_t i = 0; i < results.best.visited.size(); ++i)
-    std::cout << results.best.visited[i] << " ";
-  std::cout << std::endl;
+	// This is how the routine can be called if a lambda expression is used for
+	// the acceptance function; it's very similar for the case of a functor or a 
+	// normal function declaration.
+	Tour result =
+		genericSimulatedAnneal<Tour, unsigned long, decltype(a), decltype(s), std::mt19937>
+		(mytour, params, a, s, generator);
 
-  return 0;
+	std::cout << "Tour length: " << result.cost() << std::endl;
+	std::cout << "Computed tour: ";
+	for (auto i : result.visited)
+		std::cout << i << " ";
+	std::cout << std::endl;
+
+	return 0;
 }
--- generic_simulated_anneal.hpp
+++ generic_simulated_anneal.hpp
@@ -1,4 +1,5 @@
-/* This file copyright (c) 2016 Sean McBane, under the terms of the MIT
+/* 
+ This file copyright (c) 2017 Sean McBane, under the terms of the MIT
  License:
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -18,205 +19,169 @@
  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  SOFTWARE.
-
  */
 
 #ifndef GENERIC_SIM_ANNEALING_HPP
 #define GENERIC_SIM_ANNEALING_HPP
 
-#include <cstdlib>
 #include <iostream>
 
 #ifdef __FAST_MATH__
 #include <cmath>
 #endif /* __FAST_MATH__ */
 
-using std::size_t;
-
-/* This struct holds the results from a run of simulated annealing.
- * Fields are as follows:
- *   + best: The best solution the algorithm found.
- *   + finalcost: The final value of the cost function (for best solution).
- *   + function_evals: Number of cost function evaluations.
- *   + iterations: Number of iterations performed (where an iteration is 
- *     defined as the period between one temperature change and the next).
- */
-template<class solution, typename cost_t>
-struct GenSimAnnealResults {
-  solution best;
-  cost_t finalcost;
-  size_t function_evals;
-  size_t iterations;
+ /* This struct holds the parameters for a simulated annealing run.
+  * Fields are:
+  *   + max_temps: The maximum number of temperatures to consider.
+  *   + iters_per_temp: Iterations per temperature to compute.
+  *   + cost_reduction_tol: A tolerance for breaking out of the annealing run.
+  *     If the ratio of cost of the current solution to cost of the initial
+  *     solution is less than cost_reduction_tol, the algorithm will return.
+  *   + verbose: true for diagnostic output to stdout, false for silent operation.
+  */
+struct GenSimAnnealParams
+{
+	unsigned max_temps;
+	unsigned iters_per_temp;
+	double cost_reduction_tol;
+	bool verbose;
 };
 
-/* This struct holds the parameters for a simulated annealing run.
- * Fields are:
- *   + max_temps: The maximum number of temperatures to consider.
- *   + iters_per_temp: Iterations per temperature to compute.
- *   + alpha: The temperature reduction factor ( 0 < a < 1 )
- *   + cost_reduction_tol: A tolerance for breaking out of the annealing run.
- *     If the ratio of cost of the current solution to cost of the initial 
- *     solution is less than cost_reduction_tol, the algorithm will return.
- *   + verbose: true for diagnostic output to stdout, false for silent operation.
+/*
+ * The main simulated annealing routine. The function template has four class
+ * parameters:
+ *
+ * - A class 'solution'. An object of this type must implement 2 public methods:
+ *   void solution::perturb(), which modifies the solution slightly in place,
+ *   and cost_t solution::cost(), which returns the cost associated with this
+ *   particular solution object. This is the value the algorithm attempts to
+ *   minimize.
+ *
+ * - A class 'cost_t'. This is the type returned by solution::cost(). It should
+ *   be an integral type convertable to double.
+ *
+ * - A class acceptance_t. An object of type acceptance_t must be a functor with
+ *   signature (cost_t, cost_t, double) -> double. This functor should take as
+ *   the first argument the old cost, as a second argument the new (higher) cost,
+ *   and as the third argument the current temperature, and return a probability
+ *   0 <= p <= 1.0 that the cost increase is accepted.
+ *
+ * - A class schedule_t. An object of this type should be a functor with
+ *   signature (unsigned) -> double; it is called to calculate the temperature
+ *   at each *outer* iteration of the algorithm.
+ *
+ * - A class generator_t. This is an abstract random number generator. It 
+ *   should implement operator() to generate a random number >= 0, and max() 
+ *   to yield the maximum value of a generated number. Ex.: std::mt19937
+ *
+ * As a performance consideration, note that one copy of a 'solution' object
+ * must be made at each inner iteration. If your solution class has data that
+ * isn't changed when perturbing the solution, make sure that copy assignment
+ * of the class doesn't copy this data, e.g. by using std::shared_ptr to wrap
+ * it.
  */
-struct GenSimAnnealParams {
-  size_t max_temps;
-  size_t iters_per_temp;
-  double alpha;
-  double cost_reduction_tol;
-  bool verbose;
-};
+template<class solution, class cost_t, class acceptance_t, class schedule_t,
+	class generator_t>
+	solution genericSimulatedAnneal(const solution& s0,
+									const GenSimAnnealParams& params,
+									const acceptance_t& acceptance_f,
+									const schedule_t& cooling_schedule,
+									generator_t& generator)
+{
+	// Initialization.
+	cost_t cost_init = s0.cost();
+	cost_t cost_old = cost_init;
+	// Make a copy of the solution that was passed in:
+	solution s = s0;
+	solution s1 = s;
 
-/* Default values for the simulation parameters for convenience; some or
- * all of these may not be appropriate for a given problem. Use with caution!
- */
-const struct GenSimAnnealParams DEFAULT_SIM_ANNEALING_PARAMS = { 100, 500, 0.9,
-    0.0001, false };
+	// Keep track of current best solution;
+	solution best = s0;
+	cost_t best_cost = cost_init;
 
-/* The main routine. Templated on a 'solution' class (must be provided by the
- * user) and a class 'cost_t' which is the variable type returned by a
- * 'solution''s cost() method. This is the value that the algorithm attempts
- * to minimize.
- *
- * The third template parameter is a class which can be either a function 
- * pointer, a lambda expression or a functor. This object must take two objects
- * of type cost_t (with the second one being greater) and a double value of 
- * temperature and return a value from 0 to 1, which gives the probability that
- * a solution with the second cost is chosen over the first.
- *
- * It is important to note that this is a different approach than many other 
- * implementations, which use a fixed probability function and let the 
- * temperature function be specified on a per problem basis; in this version 
- * the temperature ALWAYS begins at 1.0 and tends to 0.
- *
- * The 'solution' class must expose two methods; one is 'perturb()', which 
- * should slightly and randomly modify the solution in place.
- * The second is 'cost()' which returns a single value of type cost_t (must be
- * an integral type).
- *
- * There are a couple of practical concerns for the user to consider; first,
- * the cost function needs to be relatively cheap to evaluate for large
- * problems as it will be evaluated millions of times. Second, and less
- * obviously, the routine must make a copy of a solution object at every
- * iteration (eithe updating it or reverting the perturbed copy to the 
- * original). This means that if your 'solution' class
- * has any data that gets deep copied during assignment (such as a std::vector) 
- * there will be a large overhead. Use pointers to any data
- * in the object that does not need to change from iteration to iteration
- * to avoid this.
- */
-template<class solution, class cost_t, class acceptance_t>
-GenSimAnnealResults<solution, cost_t> genericSimulatedAnneal(const solution& s0,
-    const GenSimAnnealParams& params, acceptance_t acceptance_f) {
+	// Iterate over temperatures (main iteration).
+	for (unsigned outer_iter = 0; outer_iter < params.max_temps; ++outer_iter) {
+		double temp = cooling_schedule(outer_iter);
+		// Inner iterations at each temperature;
+		for (unsigned inner_iter = 0; inner_iter < params.iters_per_temp;
+			 ++inner_iter) {
 
-  // Initialization.
-  size_t max_temps = params.max_temps;
-  size_t iters_per_temp = params.iters_per_temp;
-  double alpha = params.alpha;
-  double tol = params.cost_reduction_tol;
-  bool verbose = params.verbose;
-  size_t fevals = 0;
-  cost_t cost_init = s0.cost();
-  cost_t cost_old = cost_init;
+			// Perturb the solution;
+			s1.perturb();
 
-  // Make a copy of the solution that was passed in:
-  solution s = s0;
-  solution s1 = s;
+			// Whether or not we keep the new solution;
+			bool accepted = false;
 
-  // Keep track of current best solution;
-  solution best = s0;
-  cost_t best_cost = cost_init;
+			// Compute cost of new solution.
+			cost_t cost_new = s1.cost();
 
-  // Main loop.
-  double temp = 1.0;
+			// If new cost is less than old, automatically keep this solution.
+			// Also update the stored best solution.
+			if (cost_new < cost_old) {
+				accepted = true;
+				if (best_cost > cost_new) {
+					best = s1;
+					best_cost = cost_new;
+				}
+			}
 
-  // Iterate over temperatures (main iteration).
-  for (size_t outer_iter = 0; outer_iter < max_temps; ++outer_iter) {
-    // Inner iterations at each temperature;
-    for (size_t inner_iter = 0; inner_iter < iters_per_temp; ++inner_iter) {
-      // Perturb the solution;
-      s1.perturb();
+			// Otherwise, with some non-zero probability we accept the increase.
+			else {
+				double acceptance_prob = acceptance_f(cost_old, cost_new, temp);
 
-      // Whether or not we keep the new solution;
-      bool accepted = false;
-
-      // Compute cost of new solution.
-      cost_t cost_new = s1.cost();
-      fevals++;
-
-      // If new cost is less than old, automatically keep this solution.
-      // Also update the stored best solution.
-      if (cost_new < cost_old) {
-        accepted = true;
-        if (best_cost > cost_new) {
-          best = s1;
-          best_cost = cost_new;
-        }
-      }
-
-      // Otherwise, with some non-zero probability we accept the increase.
-      else {
-        double acceptance_prob = acceptance_f(cost_old, cost_new, temp);
-
-        // We don't know what happens inside of acceptance_f; must test for
-        // NaN (according to IEEE spec comparisons with NaN are always
-        // false, but -ffast-math can mess this up and I don't know how the
-        // end user will compile this). However, I can check if the macro is
-        // defined and only compile the check if fast math is on. gcc only,
-        // I don't know if this can be done for other compilers.
+				// We don't know what happens inside of acceptance_f; must test for
+				// NaN (according to IEEE spec comparisons with NaN are always
+				// false, but -ffast-math can mess this up and I don't know how the
+				// end user will compile this). However, I can check if the macro is
+				// defined and only compile the check if fast math is on. gcc only,
+				// I don't know if this can be done for other compilers.
 
 #ifdef __FAST_MATH__
-        if (std::isnan(acceptance_prob)) acceptance_prob = 0;
+				if (std::isnan(acceptance_prob)) acceptance_prob = 0;
 #endif /* __FAST_MATH__ */
 
-        double x = (double(rand())) / RAND_MAX;
-        if (acceptance_prob > x)
-          accepted = true;
-      }
+				double x = (double(generator())) / generator.max();
+				if (acceptance_prob > x)
+					accepted = true;
+			}
 
-      // If solution was accepted, update.
-      if (accepted) {
-        s = s1;
-        cost_old = cost_new;
+			// If solution was accepted, update.
+			if (accepted) {
+				s = s1;
+				cost_old = cost_new;
 
-        if (verbose)
-          std::cout << "Updating solution at outer iteration " << outer_iter
-              << ", inner iteration " << inner_iter << std::endl
-              << "  New cost: " << cost_new << std::endl;
+				if (params.verbose)
+					std::cout << "Updating solution at outer iteration " << outer_iter
+					<< ", inner iteration " << inner_iter << std::endl
+					<< "  New cost: " << cost_new << std::endl;
 
-        // Met the cost reduction criterion; break.
-        if ((double(cost_new)) / cost_init < tol) {
-          if (verbose)
-            std::cout << "Met cost reduction criterion at "
-                << "outer iteration " << outer_iter << ", inner iteration "
-                << inner_iter << ".\n";
+				// Met the cost reduction criterion; break.
+				if ((double(cost_new)) / cost_init < params.cost_reduction_tol) {
+					if (params.verbose)
+						std::cout << "Met cost reduction criterion at "
+						<< "outer iteration " << outer_iter << ", inner iteration "
+						<< inner_iter << ".\n";
 
-          GenSimAnnealResults<solution, cost_t> res = { s, s.cost(), fevals,
-              outer_iter };
-          return res;
-        }
-      }
+					return s;
+				}
+			}
 
-      // If it wasn't accepted, we have to copy s back into s1. There's no way
-      // around one copy every iteration.
-      else {
-        s1 = s;
-      }
-    }
-    temp = temp * alpha;
-  }
+			// If it wasn't accepted, we have to copy s back into s1. There's no way
+			// around one copy every iteration.
+			else {
+				s1 = s;
+			}
+		}
+	}
 
-  if (verbose)
-    std::cout << "Completed " << max_temps << " iterations without"
-        << " meeting convergence criterion;\nreturning best value found so far."
-        << std::endl;
+	if (params.verbose)
+		std::cout << "Completed " << params.max_temps << " iterations without"
+		<< " meeting convergence criterion;\nreturning best value found so far."
+		<< std::endl;
 
-  // When returning, check to see if the stored best solution is better than
-  // the current one and if it is, return it instead.
-  GenSimAnnealResults<solution, cost_t> res = {
-      s.cost() < best.cost() ? s : best,
-      s.cost() < best.cost() ? s.cost() : best.cost(), fevals, max_temps };
-  return res;
+	// When returning make sure that we didn't lose a better solution somewhere
+	// along the line.
+	return s.cost() < best.cost() ? s : best;
 }
 
 #endif /* GENERIC_SIM_ANNEALING_HPP */
